448e266f3ecf44a448a2e2c292bbbc4406057e6d
Adam Bird <adam.bird@gmail.com>
2015-02-09 17:21:33 +0000

    tests around authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,26 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe ".authenticate" do
-    let(:user) { Users(:user_1) }
-    let(:good_password) { 'password' }
-    let(:bad_password) { random_string }
+    let(:good_password) { random_string }
+    let(:user) { User.create(email: random_email, password: good_password, password_confirmation: good_password) }
+
+    subject { User.authenticate(user.email, password) }
+
+    context "when good password" do
+      let(:password) { good_password }
+
+      it "should return the user" do
+        expect(subject).to eq(user)
+      end
+    end
+
+    context "when bad password" do
+      let(:password) { random_string }
+
+      it "should be false" do
+        expect(subject).to be_falsey
+      end
+    end
   end
 
 end
